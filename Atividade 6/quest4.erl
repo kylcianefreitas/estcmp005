db92ce34c5e115b75f8ecc174ac7204c2065b1ba
Kylciane Cristiny Lopes Freitas <kclf.snf16@uea.edu.br>
2021-06-16 09:38:59 -0400

    Update quest4.erl

--- Atividade 6/quest4.erl
+++ Atividade 6/quest4.erl
@@ -1,11 +1,14 @@
+% 4.uniao: recebe duas listas que não contenham elementos repetidos e retorna uma nova com todos os elementos das duas listas originais (sem repetição)
+% ex.: uniao '(3 6 5 7) '(2 9 7 5 1)) ==> (3 6 5 7 2 9 1)
+
 -module(quest4).
 -import(lists,[append/2]).
 -export([start/0]).
 
 start() ->
-   Lst1 = [1,2,3],
-   Lst2 = [3,5,7,9],
-   Lst3 = append(Lst2,Lst1),
-   Set = sets:from_list(Lst3),
+   Lista1 = [2,9,7,5],
+   Lista2 = [3,6,5,7],
+   Lista3 = append(Lista1, Lista2),
+   Set = sets:from_list(Lista3),
    Set2 = sets:to_list(Set),
    lists:sort(Set2).
