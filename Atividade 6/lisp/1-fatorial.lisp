7ffbad4fa665ed626bfc0f5653cc6bce4652f344
Laís <laiscalderaro06@gmail.com>
2021-06-16 14:22:53 -0400

    lisp

---
+++ Atividade 6/lisp/1-fatorial.lisp
@@ -0,0 +1,7 @@
+(defun fatorial(x)
+    (if(> x 0)
+        (* x (fatorial (- x 1)))
+        1
+
+))
+(print(fatorial 5))
---
+++ Atividade 6/lisp/10-fatorialCauda.lisp
@@ -0,0 +1,9 @@
+(defun fatorial-acum (n acum)
+  (cond
+    ((<= n 1) acum)
+    (t (fatorial-acum (- n 1) (* n acum)))))
+    
+(defun fatorial (n)
+  (fatorial-acum n 1))
+
+(print (fatorial 5)) ;120
