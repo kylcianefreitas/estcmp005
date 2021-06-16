a6cd4269db9d1c2b64b1f7a4f2715537e3f7ab4b
Jose Jordan <jordanjose0410@gmail.com>
2021-06-16 11:57:07 -0400

    Ajustes nas questões

--- Atividade 6/haskell/9_compressao.hs
+++ Atividade 6/haskell/9_compressao.hs
@@ -1,8 +1,12 @@
 module Main where
 
+-- contagem_aux :: [b] -> b -> a -> (a, b)
+contagem_aux (x:lista) y cont = if (x==y) then contagem_aux lista x cont+1 else (cont, y)
+
+contagem lista = [contagem_aux lista x 0 | x <- lista]
+
+compactar lista = [x | x <- contagem lista]
 
 l1 = [2,2,2,3,4,4,2,9,5,2,4,5,5,5]
-
-
 
 main = do print(compactar l1)
